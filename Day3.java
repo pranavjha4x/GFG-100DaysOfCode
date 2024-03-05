8cf86451fe5a87f1e1880aed3645555faa6d820a
pranavjha4x <144226113+pranavjha4x@users.noreply.github.com>
2024-03-05 22:17:46 +0530

    Create Day3.java
    Given an array a of n positive integers. The task is to find the maximum of j - i subjected to the constraint of a[i] < a[j] and i < j.
    
    Example 1:
    
    Input:
    n = 2
    a[] = {1, 10}
    Output:
    1
    Explanation:
    a[0] < a[1] so (j-i) is 1-0 = 1.
    Example 2:
    
    Input:
    n = 9
    a[] = {34, 8, 10, 3, 2, 80, 30, 33, 1}
    Output:
    6
    Explanation:
    In the given array a[1] < a[7] satisfying the required condition(a[i] < a[j]) thus giving the maximum difference of j - i which is 6(7-1).
    Your Task:
    The task is to complete the function maxIndexDiff() which finds and returns maximum index difference. Printing the output will be handled by driver code. 
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(N)
    
    Constraints:
    1 ≤ n ≤ 106
    0 ≤ a[i] ≤ 109

---
+++ Day3.java
@@ -0,0 +1,35 @@
+Stack<Integer> st=new Stack<>();
+        
+        st.push(n-1);
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            if(a[i]>a[st.peek()])
+            {
+                st.push(i);
+            }
+        }
+        
+        int low=0;
+        int ans=0;
+        
+        
+        while(!st.isEmpty() && low<n)
+        {
+            if(a[st.peek()]>=a[low])
+            {
+                ans=Math.max(ans,st.pop()-low);
+            }
+            
+            else
+            {
+                low++;
+            }
+        }
+        
+        return ans;
+        
+    }
+}
+
+
